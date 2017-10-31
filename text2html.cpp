170809bcb511935613b836ba45d8fde70046a37d
Korbinian Wildwasser <korbinian.wildwasser@gmail.com>
2017-10-31 14:46:34 +0100

    refactored and commented

--- text2html.cpp
+++ text2html.cpp
@@ -5,48 +5,54 @@ using namespace std;
 
 int main (int argc, char* argv[]) {
   string line;
-  char arbiter;
+  /*status*/
   bool in_list = false;
+  /*open files*/
   ifstream infile (argv[1]);
   ofstream outfile (argv[2]);
   if (infile.is_open() && outfile.is_open())
   {
+	  /*write html head*/
 	  outfile << "<!DOCTYPE html>" << '\n' 
 		  << "<html lang=\"de\">" << '\n'
 		  << "<meta charset=UTF-8>" << '\n' 
 		  << "<body>" << '\n';
+    /*process infile line by line*/
     while ( getline (infile,line) )
     {
+	/*ignore empty lines*/
 	if(line.length() == 0) {
 		outfile << '\n';	
 		continue;
 	}
-	arbiter = line.at(0);
-	if (arbiter ==  '-') {
+	/*process lists started with a '-'*/
+	if (line.at(0) ==  '-') {
 		line.erase(0,1);
 		if(!in_list) outfile << "<ul>" << '\n';
-
 		in_list = true;
 		outfile << "<li>" << line << "</li>" << '\n';
-	} else if (isdigit(arbiter)) {
-		if(in_list) outfile << "</ul>" << '\n';
+		continue;
+	} else {
+		if(in_list) {outfile << "</ul>" << '\n';}
 		in_list = false;
+		
+	}
+	/*process headings started with a digit*/
+	if (isdigit(line.at(0))) {
 		if(isdigit(line.at(2))) {
 			outfile << "<h2>" << line << "</h2>" << '\n';
 		} else if (line.at(2) ==  ' ') {
 			outfile << "<h1>" << line << "</h1>" << '\n';
 		}
-
-	} else {
-		if(in_list) outfile << "</ul>" << '\n';
-		in_list = false;
-		outfile << "<p>" << line << "</p>" << '\n';
-
+		continue;
 	}
-      	    
+	/*default: process textlines as paragraph*/
+	outfile << "<p>" << line << "</p>" << '\n';
     }
-	if(in_list) outfile << "</ul>" << '\n';
-    outfile <<  "</body>" << '\n' << "</html>" << '\n';
+    /*close html and open lists*/
+    if(in_list) {outfile << "</ul>" << '\n';}
+    outfile <<  "</body>" << '\n' << "</html>";
+    /*close files*/
     infile.close();
     outfile.close();
   }
